60aec1a89d07e0d436e31d3c1bd460beb7465071
Pavanmum <pavanprasad035@gmail.com>
2023-06-02 15:35:04 +0530

    --app

--- src/Pages/Client.jsx
+++ src/Pages/Client.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import "../less/Client.less"
-import {CaretRightOutlined } from '@ant-design/icons';
-import { Collapse  } from 'antd';
+import React from "react";
+import "../less/Client.less";
+import { CaretRightOutlined } from "@ant-design/icons";
+import { Collapse, theme } from "antd";
 const { Panel } = Collapse;
 
 const text = `To make an animated collapsible, add max-height: 0, overflow: hidden and a transition for the max-height property, to the panel class.
@@ -9,63 +9,123 @@ const text = `To make an animated collapsible, add max-height: 0, overflow: hidd
 Then, use JavaScript to slide down the content by setting a calculated max-height, depending on the panel's height on different screen sizes`;
 
 const Client = () => {
-  
+  const { token } = theme.useToken();
+
+  const panelStyle = {
+    marginBottom: 24,
+    background: token.colorFillAlter,
+    borderRadius: token.colorBgLayout,
+  };
+
   return (
     <>
-    <div className="container">
-    <h1>FAQ For Clients</h1>
-    <Collapse className='collapse' expandIcon={({ isActive }) => <CaretRightOutlined rotate={isActive ? 90 : 0} />} >
-    <Panel header="What kind of assignments can you do?
-" key="1"  className='content'>
-      <p>{text}</p>
-    </Panel>
-    <Panel header="How do I place an order?" key="2" className='content' >
-      <p>{text}</p> 
-    </Panel>
-    <Panel header="What is the turnaround time for an assignment?" key="3" className='content'  >
-      <p>{text}</p>
-    </Panel>
-    <Panel header="How do I pay for my order?" key="4" className='content'>
-      <p>{text}</ p>
-    </Panel>
-    <Panel header="Can I communicate with the writer who is working on my  assignment?" key="5" className='content'>
-      <p>{text}</p>
-    </Panel>
-    <Panel header="Is my personal information kept confidential?" key="6" className='content' >
-      <p>{text}</p>
-    </Panel>
-    <Panel header="What if I am not satisfied with the completed assignmen t?" key="7"className='content' >
-      <p>{text}</p>
-    </Panel>
-    <Panel header="What is your refund policy?" key="8" className='content'>
-      <p>{text}</p>
-    </Panel>
-    <Panel header="Are your writers qualified to handle my assignment?" key="9" className='content'>
-      <p>{text}</p>
-    </Panel>
-    <Panel header="How do you ensure that the assignments are plagiarism-f ree?" key="10" className='content' >
-      <p>{text}</p>
-    </Panel>
-    <Panel header="How do you handle revisions?
-" key="11" >
-      <p>{text}</p>
-    </Panel>
-    <Panel header="Can y ou complete an assignment on short notice?" key="12" className='content'>
-      <p>{text}</p>
-    </Panel>
-    <Panel header="Do yo u offer discounts?" key="13"className='content' >
-      <p>{text}</p> 
-    </Panel>
-    <Panel header="Can I choose a specific writer to work on my assignment ?" key="14" className='content'>
-      <p>{text}</p>
-    </Panel>
-    <Panel header="How do I track the progress of my assignment?" key="15" className='content' >
-      <p>{text}</p>
-    </Panel>
-    </Collapse>
-    </div>
+      <div className="container">
+        <h1>FAQ For Clients</h1>
+        <Collapse
+          bordered={false}
+          className="collapse"
+          expandIcon={({ isActive }) => (
+            <CaretRightOutlined rotate={isActive ? 90 : 0} />
+          )}
+        >
+          <Panel
+            style={panelStyle}
+            header="What kind of assignments can you do?
+"
+            key="1"
+          >
+            <p>{text}</p>
+          </Panel>
+          <Panel header="How do I place an order?" key="2" style={panelStyle}>
+            <p>{text}</p>
+          </Panel>
+          <Panel
+            header="What is the turnaround time for an assignment?"
+            key="3"
+            style={panelStyle}
+          >
+            <p>{text}</p>
+          </Panel>
+          <Panel header="How do I pay for my order?" key="4" style={panelStyle}>
+            <p>{text}</p>
+          </Panel>
+          <Panel
+            header="Can I communicate with the writer who is working on my  assignment?"
+            key="5"
+            style={panelStyle}
+          >
+            <p>{text}</p>
+          </Panel>
+          <Panel
+            header="Is my personal information kept confidential?"
+            key="6"
+            style={panelStyle}
+          >
+            <p>{text}</p>
+          </Panel>
+          <Panel
+            header="What if I am not satisfied with the completed assignmen t?"
+            key="7"
+            style={panelStyle}
+          >
+            <p>{text}</p>
+          </Panel>
+          <Panel
+            header="What is your refund policy?"
+            key="8"
+            style={panelStyle}
+          >
+            <p>{text}</p>
+          </Panel>
+          <Panel
+            header="Are your writers qualified to handle my assignment?"
+            key="9"
+            style={panelStyle}
+          >
+            <p>{text}</p>
+          </Panel>
+          <Panel
+            header="How do you ensure that the assignments are plagiarism-f ree?"
+            key="10"
+            style={panelStyle}
+          >
+            <p>{text}</p>
+          </Panel>
+          <Panel
+            header="How do you handle revisions?
+"
+            key="11"
+          >
+            <p>{text}</p>
+          </Panel>
+          <Panel
+            header="Can y ou complete an assignment on short notice?"
+            key="12"
+            style={panelStyle}
+          >
+            <p>{text}</p>
+          </Panel>
+          <Panel header="Do yo u offer discounts?" key="13" style={panelStyle}>
+            <p>{text}</p>
+          </Panel>
+          <Panel
+            header="Can I choose a specific writer to work on my assignment ?"
+            key="14"
+            style={panelStyle}
+          >
+            <p>{text}</p>
+          </Panel>
+          <Panel
+            header="How do I track the progress of my assignment?"
+            key="15"
+            style={panelStyle}
+          >
+            <p>{text}</p>
+          </Panel>
+        </Collapse>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Client
+export default Client;
--- src/Pages/Seller.jsx
+++ src/Pages/Seller.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "../less/Client.less"
 import {CaretRightOutlined } from '@ant-design/icons';
-import { Collapse  } from 'antd';
+import { Collapse,theme  } from 'antd';
 const { Panel } = Collapse;
 
 const text = `To make an animated collapsible, add max-height: 0, overflow: hidden and a transition for the max-height property, to the panel class.
@@ -9,61 +9,107 @@ const text = `To make an animated collapsible, add max-height: 0, overflow: hidd
 Then, use JavaScript to slide down the content by setting a calculated max-height, depending on the panel's height on different screen sizes`;
 
 const Seller = () => {
-  
+  const { token } = theme.useToken();
+
+  const panelStyle = {
+    marginBottom: 24,
+    background: token.colorFillAlter,
+    borderRadius: token.colorBgLayout,
+
+  };
+
   return (
     <>
     <div className="container">
-    <h1>FAQ For Students</h1>
-    <Collapse className='collapse' expandIcon={({ isActive }) => <CaretRightOutlined rotate={isActive ? 90 : 0} />} >
-    <Panel header="What kind of assignments will I be working on?
-" key="1"  className='content'>
-      <p>{text}</p>
+    <h1>FAQ For 
+      Seller
+    </h1>
+    <Collapse bordered={false} className='collaspse' expandIcon={({ isActive }) => <CaretRightOutlined rotate={isActive ? 90 : 0} />} >
+    <Panel style={panelStyle} header="What kind of assignments will I be working on?
+" key="1"  >
+      <p className='cont'>{text}</p>
     </Panel>
-    <Panel header="How do I get started on a new assignment?" key="2" className='content' >
+    </Collapse>
+    <Collapse  bordered={false} className='collaspse' expandIcon={({ isActive }) => <CaretRightOutlined rotate={isActive ? 90 : 0} />} >
+     <Panel style={panelStyle} header="How do I get started on a new assignment?" key="2" >
       <p>{text}</p> 
     </Panel>
-    <Panel header="How much will I get paid for completing an assignment?" key="3" className='content'  >
+    </Collapse>
+    <Collapse   bordered={false} className='collaspse' expandIcon={({ isActive }) => <CaretRightOutlined rotate={isActive ? 90 : 0} />} >
+    <Panel header="How much will I get paid for completing an assignment?" key="3" style={panelStyle}  >
       <p>{text}</p>
     </Panel>
-    <Panel header="What is the turnaround time for completing an assignment?" key="4" className='content'>
+    </Collapse>
+
+    <Collapse   bordered={false} className='collaspse' expandIcon={({ isActive }) => <CaretRightOutlined rotate={isActive ? 90 : 0} />} >
+    <Panel header="What is the turnaround time for completing an assignment?" key="4" style={panelStyle}>
       <p>{text}</ p>
     </Panel>
-    <Panel header="How will I receive payment for completed assignments?" key="5" className='content'>
+    </Collapse>
+
+    <Collapse  bordered={false} className='collaspse' expandIcon={({ isActive }) => <CaretRightOutlined rotate={isActive ? 90 : 0} />} >
+    <Panel style={panelStyle} header="How will I receive payment for completed assignments?" key="5" >
       <p>{text}</p>
     </Panel>
-    <Panel header="What happens if I am unable to complete an assignment?" key="6" className='content' >
+    </Collapse>
+
+    <Collapse  bordered={false} className='collaspse' expandIcon={({ isActive }) => <CaretRightOutlined rotate={isActive ? 90 : 0} />} >
+    <Panel header="What happens if I am unable to complete an assignment?" key="6" style={panelStyle} >
       <p>{text}</p>
     </Panel>
+    </Collapse>
+
+    <Collapse  bordered={false} className='collaspse' expandIcon={({ isActive }) => <CaretRightOutlined rotate={isActive ? 90 : 0} />} >
     <Panel header="Will I receive credit for my work?
-" key="7"className='content' >
+" key="7"style={panelStyle} >
       <p>{text}</p>
     </Panel>
+    </Collapse>
+
+    <Collapse  bordered={false} className='collaspse' expandIcon={({ isActive }) => <CaretRightOutlined rotate={isActive ? 90 : 0} />} >
     <Panel header="How do I communicate with the client if I have questions or need clarification?
-" key="8" className='content'>
+" key="8" style={panelStyle}>
       <p>{text}</p>
     </Panel>
-    <Panel header="How do I ensure that the completed assignment is original and plagiarism-free?" key="9" className='content'>
+    </Collapse>
+
+    <Collapse  bordered={false} className='collaspse' expandIcon={({ isActive }) => <CaretRightOutlined rotate={isActive ? 90 : 0} />} >
+    <Panel header="How do I ensure that the completed assignment is original and plagiarism-free?" key="9" style={panelStyle}>
       <p>{text}</p>
     </Panel>
-    <Panel header="Will I have access to reference materials and resources for completing the assignment?" key="10" className='content' >
+    </Collapse>
+
+    <Collapse   bordered={false} className='collaspse' expandIcon={({ isActive }) => <CaretRightOutlined rotate={isActive ? 90 : 0} />} >
+    <Panel header="Will I have access to reference materials and resources for completing the assignment?" key="10" style={panelStyle} >
       <p>{text}</p>
     </Panel>
+    </Collapse>
+
+    <Collapse  bordered={false} className='collaspse' expandIcon={({ isActive }) => <CaretRightOutlined rotate={isActive ? 90 : 0} />} >
     <Panel header="Can I choose which assignments I want to work on?
 
 " key="11" >
       <p>{text}</p>
     </Panel>
+    </Collapse>
+    <Collapse  bordered={false} className='collaspse' expandIcon={({ isActive }) => <CaretRightOutlined rotate={isActive ? 90 : 0} />} >
     <Panel header="How do I know if I am qualified to work on a particular assignment?
-" key="12" className='content'>
+" key="12" style={panelStyle}>
       <p>{text}</p>
     </Panel>
-    <Panel header="What if the client is not satisfied with my completed work?" key="13"className='content' >
+    </Collapse>
+    <Collapse  bordered={false} className='collaspse' expandIcon={({ isActive }) => <CaretRightOutlined rotate={isActive ? 90 : 0} />} >
+    <Panel header="What if the client is not satisfied with my completed work?" key="13"style={panelStyle} >
       <p>{text}</p> 
     </Panel>
-    <Panel header="Can I choose a specific writer to work on my assignment ?" key="14" className='content'>
+    </Collapse>
+    <Collapse  bordered={false} className='collaspse' expandIcon={({ isActive }) => <CaretRightOutlined rotate={isActive ? 90 : 0} />} >
+    <Panel header="Can I choose a specific writer to work on my assignment ?" key="14" style={panelStyle}>
       <p>{text}</p>
     </Panel>
-    <Panel header="Is my personal information kept confidential?" key="15" className='content' >
+    </Collapse>
+    <Collapse  bordered={false} className='collaspse' expandIcon={({ isActive }) => <CaretRightOutlined rotate={isActive ? 90 : 0} />} >
+    <Panel header="Is my personal information kept confidential?" key="15" style={panelStyle} >
       <p>{text}</p>
     </Panel>
     </Collapse>
